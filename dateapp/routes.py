ee579ac85f30504b36584aadab0e49b7fc22d782
YanLin Zhu <yzhu000@citymail.cuny.edu>
2017-10-30 16:20:04 -0400

    updated routes.py

--- dateapp/routes.py
+++ dateapp/routes.py
@@ -12,4 +12,26 @@ def index():
 
     userinput = request.args.get('userdate', 0)
 
-    
+    if userinput:
+      InputYear, InputMonth, InputDay = (int(x) for x in str(userinput).split('-'))
+      Input_weekday = datetime.date(InputYear, InputMonth, InputDay).weekday()
+      if Input_weekday == 0:
+           Input_weekday = "Monday"
+      elif Input_weekday == 1:
+           Input_weekday = "Tudesday"
+      elif Input_weekday == 2:
+           Input_weekday = "Wednesday"
+      elif Input_weekday == 3:
+           Input_weekday = "Thursday"
+      elif Input_weekday == 4:
+           Input_weekday = "Friday"
+      elif Input_weekday == 5:
+           Input_weekday = "Saturday"
+      else:
+           Input_weekday = "Sunday"
+
+      return render_template("index.html", TitleName = TitleName, HeadName = HeadName,
+      Input_weekday = Input_weekday, InputYear = InputYear, InputMonth = InputMonth, InputDay = InputDay)
+
+    else:
+       return render_template("index.html", TitleName = TitleName, HeadName = HeadName)
