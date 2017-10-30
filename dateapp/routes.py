b5eec2fe752b664f3406b4ee951987d12660df66
YanLin Zhu <yzhu000@citymail.cuny.edu>
2017-10-30 15:44:18 -0400

    set up routes.py

--- dateapp/routes.py
+++ dateapp/routes.py
@@ -2,3 +2,14 @@ from . import app
 import datetime
 from flask import request
 from flask import render_template
+
+
+@app.route('/', methods=['GET'])
+def index():
+
+    TitleName = "Day of the Week Calculator"
+    HeadName = "Day of the Week Calculator"
+
+    userinput = request.args.get('userdate', 0)
+
+    
